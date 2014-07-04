3fba767a365b3d290fc32c877c37ce1f9f8948af
Mike Pennisi <mike@mikepennisi.com>
2014-07-04 18:30:12 -0400

    More work

--- jq.js
+++ jq.js
@@ -1,14 +1,26 @@
-var Node = require('./dom-node');
+var Node = require('./lib/dom-node');
 
-var document = new Node();
+var document = new Node({
+	nodeType: 9
+});
+document.documentElement = new Node({
+	tagName: 'html',
+	ownerDocument: document
+});
 document.createElement = function(tagName) {
-	return new Node({ parent: this });
+	return new Node({
+		tagName: tagName,
+		parent: this,
+		ownerDocument: document
+	});
 };
 document.removeChild = function(childNode) {
 
 };
 document.createDocumentFragment = function() {
-	return new Node();
+	return new Node({
+		nodeType: 11
+	});
 };
 
 var $ = require('jquery')({
@@ -16,4 +28,9 @@ var $ = require('jquery')({
 	addEventListener: function() {}
 });
 
-console.log($('<div>'));
+var $statement = $("<div>")
+	.html("The <b>future</b> is <span>something</span>.")
+	.find('span').text('cool')
+	.end();
+
+console.log($statement.html());
