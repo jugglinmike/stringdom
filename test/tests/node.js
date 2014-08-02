3148b6fd5e804141a680794b517a947f07469642
Mike Pennisi <mike@mikepennisi.com>
2014-08-01 20:14:00 -0400

    Implement `Node#firstChild`

--- test/tests/node.js
+++ test/tests/node.js
@@ -12,6 +12,34 @@ suite('Node', function() {
 		assert.deepEqual(parent.childNodes, [child]);
 	});
 
+	suite('#firstChild', function() {
+		var parent;
+
+		setup(function() {
+			parent = new Node();
+		});
+
+		test('no children', function() {
+			assert.equal(parent.firstChild, null);
+		});
+
+		test('one child', function() {
+			var child = new Node();
+			parent.appendChild(child);
+
+			assert.equal(parent.firstChild, child);
+		});
+
+		test('many children', function() {
+			var firstChild = new Node();
+			var lastChild = new Node();
+			parent.appendChild(firstChild);
+			parent.appendChild(lastChild);
+
+			assert.equal(parent.firstChild, firstChild);
+		});
+	});
+
 	suite('#lastChild', function() {
 		var parent;
 
