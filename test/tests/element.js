1ea7d4957f20d8472672bcd52d03be4b1c7704b1
Mike Pennisi <mike@mikepennisi.com>
2014-09-26 17:08:20 -0400

    Prototype implementation of `Node#ownerDocument`

--- test/tests/element.js
+++ test/tests/element.js
@@ -13,4 +13,15 @@ suite('Element', function() {
 		var elem = create('<div class="a b c">');
 		assert.equal(elem.className, 'a b c');
 	});
+
+	suite('innerHTML', function() {
+		test('`ownerDocument` attribute', function() {
+			var elem = create('<div>');
+			elem.ownerDocument = {};
+
+			elem.innerHTML = '<span></span>';
+
+			assert.equal(elem.ownerDocument, elem.childNodes[0].ownerDocument);
+		});
+	});
 });
