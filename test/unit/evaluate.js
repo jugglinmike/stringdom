ab1df6a652cf8484243377886f283a69bce62d0e
Mike Pennisi <mike@mikepennisi.com>
2014-10-28 15:41:46 -0400

    Correct linkage of parsed text nodes

---
+++ test/unit/evaluate.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var evaluate = require('../../lib/evaluate');
+
+suite('evaluate', function() {
+	suite('text nodes', function() {
+		test('parent of first child', function() {
+			var dom = evaluate('<div>text</div>', {});
+			var div = dom[0];
+			var text = div.childNodes[0];
+
+			assert.equal(div.childNodes.length, 1);
+			assert.equal(text.nodeType, 3);
+			assert.equal(text.parentNode, div);
+		});
+
+		test('parent of subsequent children', function() {
+			var dom = evaluate('<div><h1></h1>text</div>', {});
+			var div = dom[0];
+			var text = div.childNodes[1];
+
+			assert.equal(div.childNodes.length, 2);
+			assert.equal(text.nodeType, 3);
+			assert.equal(text.parentNode, div);
+		});
+	});
+});
