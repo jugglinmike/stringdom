ec40b71ef39afc97c9dcae45a1e0ccbe3efd2336
Mike Pennisi <mike@mikepennisi.com>
2014-10-28 15:46:28 -0400

    Correct linkage of parsed comment nodes

--- test/unit/evaluate.js
+++ test/unit/evaluate.js
@@ -24,4 +24,26 @@ suite('evaluate', function() {
 			assert.equal(text.parentNode, div);
 		});
 	});
+
+	suite('comment nodes', function() {
+		test('parent of first child', function() {
+			var dom = evaluate('<div><!-- comment --></div>', {});
+			var div = dom[0];
+			var comment = div.childNodes[0];
+
+			assert.equal(div.childNodes.length, 1);
+			assert.equal(comment.nodeType, 8);
+			assert.equal(comment.parentNode, div);
+		});
+
+		test('parent of subsequent children', function() {
+			var dom = evaluate('<div><h1></h1><!-- comment --></div>', {});
+			var div = dom[0];
+			var comment = div.childNodes[1];
+
+			assert.equal(div.childNodes.length, 2);
+			assert.equal(comment.nodeType, 8);
+			assert.equal(comment.parentNode, div);
+		});
+	});
 });
